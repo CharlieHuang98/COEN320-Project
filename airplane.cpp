91d6e59d09bf794f70875e5ba6f6bf724896bfbe
Kas73 <kasrakamjoo@live.com>
2023-03-23 13:13:44 -0400

    Adding basics for airplane cpp and h

--- airplane.cpp
+++ airplane.cpp
@@ -1,11 +1,49 @@
-#include <iostream>
-#include <string.h>
+//airplane.cpp
+
+#include "airplane.h"
 using namespace std;
 
+//constructor
+airplane::airplane(int id, int arrival_t, int departure_t, int position[3], int speed[3]){
+	Id = id;
+	Arrival_t = arrival_t;
+	Departure_t = departure_t;
+	for (int i=0; i<3; i++){
+		Position[i] = position[i];
+		Speed[i] = speed[i];
+	}
 
+}
 
+//destructor
+airplane::~airplane(){
 
-int main_airplane() {
-	cout << "Hello World!!!" << endl; // prints Hello World!!!
-	return 0;
+}
+
+//Getters
+int airplane::getId() const{
+	return Id;
+}
+int airplane::getArrival(){
+	return Arrival_t;
+}
+int airplane::getDeparture(){
+	return Departure_t;
+}
+int* airplane::getPosition(){
+	return Position;
+}
+int* airplane::getSpeed(){
+	return Speed;
+}
+//Setters
+void airplane::setPosition(int x,int y, int z){
+	Position[0]=x;
+	Position[1]=y;
+	Position[2]=z;
+}
+void airplane::setSpeed(int x, int y, int z){
+	Speed[0]=x;
+	Speed[1]=y;
+	Speed[2]=z;
 }
--- airplane.h
+++ airplane.h
@@ -1,22 +1,53 @@
 /*
  * airplane.h
  *
- *  Created on: Mar. 19, 2023
- *      Author: Charlie Huang
+ *  Created on: Mar. 23, 2023
+ *      Author: kasra
  */
 
-#ifndef SRC_AIRPLANE_H_
-#define SRC_AIRPLANE_H_
+#ifndef AIRPLANE_H_
+#define AIRPLANE_H_
+
+#include <stdio.h>
+#include <iostream>
+#include <time.h>
+
+#include <sync.h>
+#include <sys/siginfo.h>
+#include <sys/neutrino.h>
+#include <sys/netmgr.h>
+#include <sys/syspage.h>
+#include <inttypes.h>
+#include <stdint.h>
 
 class airplane {
-	int ID;
-	Vector Position;
-	Vector Speed;
+public:
+	//constructor and destructor
+	airplane(int id, int arrival_t, int departure_t, int position[3], int speed[3]);
+	~airplane();
 
-	Public:
 	bool airplane_entering = false; // bool value will be set to true when the airplane is actually entering the airspace
 	bool airplane_exiting = false; // bool value will be set to true when the airplane is actually exiting the airspace
-	long timestamp = 0; // time of last update for the airplane
+	long timestamp = 0;
+
+	//functions below
+	//getters
+	int getId() const;
+	int getArrival();
+	int getDeparture();
+	int* getPosition();
+	int* getSpeed();
+	//setters
+	void setPosition(int x, int y, int z);
+	void setSpeed(int x, int y, int z);
+
+
+private:
+	int Id;
+	int Arrival_t;
+	int Departure_t;
+	int Position[3];
+	int Speed[3];
 };
 
-#endif /* SRC_AIRPLANE_H_ */
+#endif /* AIRPLANE_H_ */
