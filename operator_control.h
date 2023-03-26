94383b67e8d97f4663718924ac58ec17a369f7ed
Charlie Huang <74630670+CharlieHuang98@users.noreply.github.com>
2023-03-26 00:14:34 -0400

    Update operator_control.h
    Added functions to make operator.cpp work

--- operator_control.h
+++ operator_control.h
@@ -10,8 +10,19 @@
 
 #include <iostream>
 #include <string.h>
-using namespace std;
+#include <time.h>
+#include <stdlib.h>
 
 #include "computer_system.h"
+
+class operator_control {
+public:
+    operator_control();
+    void requestAircraftInfo(int airplane_id, int airplane_speed, int airplane_position);
+    void changeAirplaneFlightLevel(int new_flight_level);
+    void changeAirplanePosition(int new_position_x, int new_position_y, int new_position_z);
+    void changeAirplaneSpeed(int new_speed);
+
+};
 
 #endif /* SRC_OPERATOR_CONTROL_H_ */
