abc4dcdec62704c2b22e25245b6f833667fcf751
Charlie Huang <74630670+CharlieHuang98@users.noreply.github.com>
2023-04-09 01:16:29 -0400

    Updated some stuff
    Server needs to be from TCP to Threads MsgSend

--- operator_control.cpp
+++ operator_control.cpp
@@ -1,45 +1,92 @@
 /*
- * operator_control.cpp
+ * computer_system.cpp
  *
- *  Created on: Mar. 19, 2023
- *      Author: Huang
+ *  Created on: Mar. 5, 2023
+ *      Author:
+ *      Charlie Huang
+ *
  */
+
 
 #include <iostream>
 #include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 using namespace std;
 
-#include "computer_system.cpp"
 #include "operator_control.h"
-
 
 class OperatorController {
 public:
-
-    OperatorController(){
-        // add connection to computer system and data display
-    }
+    OperatorController() {}
 
     void requestAircraftInfo(int airplane_id, int airplane_speed, int airplane_position){
-        // send request to computer system to get aircraft ID
-        // Update Data Display
+        string message = "get more aircraft information " + to_string(airplane_id) + " " + to_string(airplane_speed) + " " + to_string(airplane_position) + "\n";
+        sendMessageToServer(message);
     }
 
     void changeAirplaneFlightLevel(int new_flight_level){
-        string command = "change flight level" + to_string(new_flight_level);
-        // Send command to computer system to change the airplane's flight level
+        string message = "change flight level " + to_string(new_flight_level) + "\n";
+        sendMessageToServer(message);
     }
 
     void changeAirplanePosition(int new_position_x, int new_position_y, int new_position_z){
-        string command = "change position" + to_string(new_position_x) + " " + to_string(new_position_y) + " " + to_string(new_position_z) + "\n";
-        // Send command to computer system to change the airplane's position
+        string message = "change position " + to_string(new_position_x) + " " + to_string(new_position_y) + " " + to_string(new_position_z) + "\n";
+        sendMessageToServer(message);
     }
 
     void changeAirplaneSpeed(int new_speed){
-        string command = "change speed" + to_string(new_speed) + "\n";
-        // Send command to computer system to change the airplane's speed
+        string message = "change speed " + to_string(new_speed) + "\n";
+        sendMessageToServer(message);
+    }
+
+
+    // this is wrong, needs to use MsgSend not creating a TCP socket
+    /*
+private:
+    void sendMessageToServer(const string& message) {
+        // create a TCP socket
+        int sock = socket(AF_INET, SOCK_STREAM, 0);
+        if (sock < 0) {
+            cerr << "Error creating socket" << endl;
+            return;
+        }
+
+        // specify the server address and port number
+        struct sockaddr_in serverAddr;
+        memset(&serverAddr, 0, sizeof(serverAddr));
+        serverAddr.sin_family = AF_INET;
+        serverAddr.sin_port = htons(12345); // replace with the actual port number used by the server side
+        serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1"); // replace with the actual IP address of the server side
+
+        // connect to the server
+        if (connect(sock, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
+            cerr << "Error connecting to server" << endl;
+            close(sock);
+            return;
+        }
+
+        // send the message to the server
+        if (send(sock, message.c_str(), message.length(), 0) < 0) {
+            cerr << "Error sending message to server" << endl;
+            close(sock);
+            return;
+        }
+
+        // receive the response from the server (optional)
+        char response[1024];
+        if (recv(sock, response, sizeof(response), 0) < 0) {
+            cerr << "Error receiving response from server" << endl;
+        } else {
+            cout << "Server response: " << response << endl;
+        }
+
+        // close the socket
+        close(sock);
     }
 };
+*/
 
 enum Available_Commands{
     change_flight_level,
@@ -57,35 +104,51 @@ int main(){
      * The first while loop is to change the airplane's speed or position.
      */
     while(true){
-        cout << "Enter a command to get or change the airplane's Flight Level, Speed or Position: ";
-        string command;
-        getline(cin, command);
+    cout << "Enter a command to get or change the airplane's Flight Level, Speed or Position: ";
+    string command;
+    getline(cin, command);
 
-        if(command == "change position"){
-            int new_position_x, new_position_y, new_position_z;
-            cout << "Enter a new position (x, y, z): ";
-            cin >> new_position_x >> new_position_y >> new_position_z;
-            cin.ignore(); // ignore the newline character left in the input buffer
-            operator_controller.changeAirplanePosition(new_position_x, new_position_y, new_position_z);
-            user_input = Available_Commands::change_position;
-        } else if(command == "change speed"){
-            int new_speed;
-            cout << "Enter a new speed for the airplane: ";
-            cin >> new_speed;
-            cin.ignore(); // ignore the newline character left in the input buffer
-            operator_controller.changeAirplaneSpeed(new_speed);
-            user_input = Available_Commands::change_speed;
-        } else if(command == "change flight level"){
-            int new_flight_level;
-            cout << "Enter a new flight level for the airplane: ";
-            cin >> new_flight_level;
-            cin.ignore(); // ignore the newline character left in the input buffer
-            operator_controller.changeAirplaneFlightLevel(new_flight_level);
-            user_input = Available_Commands::change_flight_level;
-        } else {
-            user_input = Available_Commands::invalid;
-            cout << "the command entered was invalid" << endl;
-        }
+    if(command == "change position"){
+        int new_position_x, new_position_y, new_position_z;
+        cout << "Enter a new position (x, y, z): ";
+        cin >> new_position_x >> new_position_y >> new_position_z;
+        cin.ignore(); // ignore the newline character left in the input buffer
+        operator_controller.changeAirplanePosition(new_position_x, new_position_y, new_position_z);
+        user_input = Available_Commands::change_position;
+    } else if(command == "change speed"){
+        int new_speed;
+        cout << "Enter a new speed for the airplane: ";
+        cin >> new_speed;
+        cin.ignore(); // ignore the newline character left in the input buffer
+        operator_controller.changeAirplaneSpeed(new_speed);
+        user_input = Available_Commands::change_speed;
+    } else if(command == "change flight level"){
+        int new_flight_level;
+        cout << "Enter a new flight level for the airplane: ";
+        cin >> new_flight_level;
+        cin.ignore(); // ignore the newline character left in the input buffer
+        operator_controller.changeAirplaneFlightLevel(new_flight_level);
+        user_input = Available_Commands::change_flight_level;
+    } else if(command == "more information"){
+    	string more_info = "more information";
+    	cout << "More information was requested";
+    	cin >> more_info;
+    	user_input = Available_Commands::more_information;
+
+    	// Send message to server to request more aircraft information
+    	MsgToComputerSystem msg;
+    	msg.id = MORE_AIRCRAFT_INFO;
+    	if (operator_controller.sendMsgToComputer_System(msg)) {
+    		cout << "Message sent successfully!" << endl;
+    	} else {
+    		cout << "Failed to send message to server." << endl;
+    	}
     }
+    else {
+        user_input = Available_Commands::invalid;
+        cout << "the command entered was invalid" << endl;
+    }
+
     return 0;
 }
+
