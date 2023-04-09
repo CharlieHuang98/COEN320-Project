5ad31565c001f4ec2fcff24620893988dab398d2
Charlie Huang <74630670+CharlieHuang98@users.noreply.github.com>
2023-04-09 01:17:03 -0400

    updated the class

--- operator_control.h
+++ operator_control.h
@@ -1,19 +1,40 @@
 /*
  * operator_control.h
  *
- *  Created on: Mar. 19, 2023
+ *  Created on: Mar. 30, 2023
  *      Author: Huang
  */
 
 #ifndef SRC_OPERATOR_CONTROL_H_
 #define SRC_OPERATOR_CONTROL_H_
 
+
 #include <iostream>
 #include <string.h>
 #include <time.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <sys/dispatch.h>
+#include <pthread.h>
 
 #include "computer_system.h"
+
+typedef struct _pulse msg_header_t;
+
+typedef enum {OperatorSetCalculationInterval, AirplaneRadarUpdate, ClockTimerCompUpdate} MsgToComputerSystemType;
+
+typedef struct{
+		msg_header_t hdr;
+		MsgToComputerSystemType type;
+		int id;
+		int calculationInterval;
+		double x, y, z;
+		double dx, dy, dz;
+		long timestamp;
+		bool isAirplaneEntering;
+		bool isAirplaneLeaving;
+} MsgToComputerSystem;
 
 class operator_control {
 public:
@@ -22,6 +43,8 @@ public:
     void changeAirplaneFlightLevel(int new_flight_level);
     void changeAirplanePosition(int new_position_x, int new_position_y, int new_position_z);
     void changeAirplaneSpeed(int new_speed);
+
+    bool sendMsgToComputer_System(MsgToComputerSystem msg);
 
 };
 
