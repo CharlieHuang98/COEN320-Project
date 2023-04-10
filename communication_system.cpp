cea2515f2372783cd7ab79f88183ebaf4b4fd8d0
Charlie Huang <74630670+CharlieHuang98@users.noreply.github.com>
2023-04-09 22:38:53 -0400

    Almost Finalized
    This is almost done, all that it's missing is fixing error and testing with Airplane and Communication System.

--- communication_system.cpp
+++ communication_system.cpp
@@ -17,6 +17,7 @@ using namespace std;
 
 #include "communication_system.h"
 #include "computer_system.cpp"
+#include "System_Specifications.h"
 
 
 #define ATTACH_POINT "communication_system_channel" // change ATTACH_POINT to server name
@@ -26,13 +27,15 @@ using namespace std;
 /* This struct contains a type/subtype field as the first 4 bytes.
  * The type and subtype field can be used to identify the message that is being received.
 / * This allows you to identify data which isn't destined for your server. */
+
 typedef struct _pulse msg_header_t;
 
 /* Our real data comes after the header */
 typedef struct communication_system_data {
 	msg_header_t hdr;
 	double data;
-} communication_system_data;
+	MsgToAirplane type;
+} my_data_t;
 
 
 
@@ -41,24 +44,26 @@ typedef struct communication_system_data {
  */
 
 pthread_t initializeCommunicationSystemThread(){
-    int x1;
-	pthread_t Communication_thread_ID;
+    int err_no;
+	pthread_t Communication_threadD;
 	pthread_attr_t attr;
 	int policy;
 	int detachstate;
 
-	x1 = pthread_attr_init(&attr);
-	if(x1)
-		LOG_ERROR("ERROR, RC from pthread_attr_init() is %d \n", rc);
+	err_no = pthread_attr_init(&attr);
+	if(err_no)
+		ERROR("ERROR, RC from pthread_attr_init() is %d \n", err_no);
+
+	// create thread with attributes
+		err_no = pthread_create(&Communication_threadD, &attr, communication_system_main, NULL);
 
 	// detach state
-	x1 = pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+	err_no = pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+	if(err_no)
+			ERROR("ERROR, RC from pthread_attr_init() is %d \n", err_no);
 
 
-	// thread with attributes
-	x1 = pthread_create(&Communication_thread_ID, &attr, NULL); // removed CommSys_main
-
-return thread;
+return Communication_threadD;
 }
 
 
@@ -66,38 +71,26 @@ return thread;
  *
  * There are still a lot of errors that need to be fixed
  */
-bool isCompSystemMessageOK(){
-	int rcvid;
-	const MsgToCommunicationSystem & msg;
-
-	/* Create a local name (/dev/name/local/...) */
-	if ((attach = name_attach(NULL, ATTACH_POINT, 0)) == NULL) {
-		perror("Error occurred while creating the channel");
-	}
+bool isCompSystemMessageGood(int rcvid, my_data_t MsgToCommunicationSystem){
 
 	/* Do your MsgReceive's here now with the chid */
-	while (true) {
 		/* Server will block in this call, until a client calls MsgSend to send a message to
 		 * this server through the channel named "myname", which is the name that we set for the channel,
 		 * i.e., the one that we stored at ATTACH_POINT and used in the name_attach call to create the channel. */
-		rcvid = MsgReceive(attach->chid, &msg, sizeof(msg), NULL);
-
-		/* In the above call, the received message will be stored at msg when the server receives a message.
-		 * Moreover, rcvid */
 
 		if (rcvid == -1) {/* Error condition, exit */
-			break;
+			return false;
 		}
 
 		if (rcvid == 0) {/* Pulse received */
-			switch (msg.hdr.code) {
+			switch (MsgToCommunicationSystem.hdr.code) {
 			case _PULSE_CODE_DISCONNECT:
 				/*
 				 * A client disconnected all its connections (called
 				 * name_close() for each name_open() of our name) or
 				 * terminated
 				 */
-				ConnectDetach(msg.hdr.scoid);
+				ConnectDetach(MsgToCommunicationSystem.hdr.scoid);
 				break;
 			case _PULSE_CODE_UNBLOCK:
 				/*
@@ -114,19 +107,19 @@ bool isCompSystemMessageOK(){
 				 */
 				break;
 			}
-			continue;
+			return false;
 		}
 
 		/* name_open() sends a connect message, must EOK this */
-		if (msg.hdr.type == _IO_CONNECT ) {
+		if (MsgToCommunicationSystem.hdr.type == _IO_CONNECT ) {
 			MsgReply( rcvid, EOK, NULL, 0 );
-			continue;
+			return false;
 		}
 
 		/* Some other QNX IO message was received; reject it */
-		if (msg.hdr.type > _IO_BASE && msg.hdr.type <= _IO_MAX ) {
+		if (MsgToCommunicationSystem.hdr.type > _IO_BASE && MsgToCommunicationSystem.hdr.type <= _IO_MAX ) {
 			MsgError( rcvid, ENOSYS );
-			continue;
+			return false;
 		}
 
 		/* Here are the messages that you will work on, i.e., messages that will have a meaning for
@@ -146,44 +139,42 @@ bool isCompSystemMessageOK(){
 		 * case because the server will block when waiting for a message, i.e., it will not be hogging CPU.). Then, the
 		 * configuratin message could be used to send the period/frequency in which the client would be sending messages.
 		 * Thus, the server could consider that while doing other things. */
-		if (msg.hdr.type == 0x00) {
+
+		if (MsgToCommunicationSystem.hdr.type == 0x00) {
 			/*A motion message has been received*/
-			if (msg.hdr.subtype == 0x01) {
+			if (MsgToCommunicationSystem.hdr.subtype == 0x01) {
 				/* A velocity message has been received  */
-				printf("The velocity of the object is  %.2f m/s \n", msg.data);
+				printf("The velocity of the object is  %.2f m/s \n", MsgToCommunicationSystem.data);
 			}
-			else if (msg.hdr.subtype == 0x02) {
+			else if (MsgToCommunicationSystem.hdr.subtype == 0x02) {
 				/* A position message has been received  */
-				printf("The x position of the object is  %.2f m \n", msg.data);
+				printf("The x position of the object is  %.2f m \n", MsgToCommunicationSystem.data);
 				printf("\n");
 			}
 			else{
 				MsgError( rcvid, ENOSYS );
-				continue;
+				return false;
 			}
 		}
 
 		MsgReply(rcvid, EOK, 0, 0);
 
-	}
-
-	/* Remove the name from the space */
-	name_detach(attach, 0);
-
 	return NULL;
 }
 
-static bool SendCommandToAirplane(MessageToAirplane msg){
+// issue here on how to send message to airplane since airplane doesn't have a channel
+static bool SendCommandToAirplane(){
 	int server_coid; //server connection ID
 	const char* AirplaneChannel;
+	my_data_t MessageToAirplane;
 
 
 	if ((server_coid = name_open(AirplaneChannel, 0)) == -1) {
 			perror("Error occurred while attaching the channel");
-			break;
+			return false;
 		}
 
-	if(MsgSend(server_coid, &msg, sizeof(msg), NULL, 0) == -1){
+	if(MsgSend(server_coid, &MessageToAirplane, sizeof(MessageToAirplane), NULL, 0) == -1){
 		printf("Error while sending the position message");
 		return false;
 	}
@@ -191,12 +182,68 @@ static bool SendCommandToAirplane(MessageToAirplane msg){
 	/* Close the connection */
 		name_close(server_coid);
 		return true;
-	}
+}
 
 void* communication_system_main(void* arg){
 	int rcvid;
+    name_attach_t *attach;
+    my_data_t msg;
+
+    char CommunicationSystem_Channel[64] = "CommunicationSystemChannel";
+
+
+    /* Create a local name (/dev/name/local/...) */
+    if ((attach = name_attach(NULL, ATTACH_POINT, 0)) == NULL) {
+    	ERROR("Error occurred while creating the channel", CommunicationSystem_Channel);
+    	return NULL;
+    }
+
+    while (true) {
+    		/* Server will block in this call, until a client calls MsgSend to send a message to
+    		 * this server through the channel named "myname", which is the name that we set for the channel,
+    		 * i.e., the one that we stored at ATTACH_POINT and used in the name_attach call to create the channel. */
+    		rcvid = MsgReceive(attach->chid, &msg, sizeof(msg), NULL);
+
+    		if (rcvid == -1) {/* Error condition, exit */
+    			break;
+    			}
+    		bool CompSystemMessageGood = isCompSystemMessageGood(rcvid, msg);
+
+
+    	// here the code should continue to handle the message sent to the airplane but the airplane.cpp doesn't receive messages
+
+    		if(msg.type == ChangeAirplaneSpeed){
+    			MsgToAirplane msgToAirplane;
+    			msgToAirplane.id =  msg.id;
+    			msgToAirplane.type = ChangeAirplaneSpeed;
+
+    			// Set for Coordinates
+    			msgToAirplane.PosX = msg.PosX;
+    			msgToAirplane.PosY = msg.PosY;
+    			msgToAirplane.PosZ = msg.PosZ;
+
+    			// Set for Speed
+    			msgToAirplane.SpeedX = msg.SpeedX;
+    			msgToAirplane.SpeedY = msg.SpeedY;
+    			msgToAirplane.SpeedZ = msg.SpeedZ;
+
+    			MsgReply(rcvid, EOK, 0, 0);
+
+
+    		}else if(msg.type == RequestAirplaneData){
+    			MsgToAirplane msgToAirplane;
+    			msgToAirplane.id =  msg.id;
+    			msgToAirplane.type = RequestAirplaneData;
+
+    			MsgReply(rcvid, EOK, 0, 0);
+
+
+    		} else{
+    			MsgReply(rcvid, EOK, 0, 0);
+    		}
+    		return EXIT_SUCCESS;
+    }
 
 }
-
 
 
