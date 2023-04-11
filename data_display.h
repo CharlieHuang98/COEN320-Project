645e85eb897b0fee6d2673e6797c0d37b726ef41
tchardino <47332962+tchardino@users.noreply.github.com>
2023-04-11 00:36:00 -0400

    Update data_display.h

--- data_display.h
+++ data_display.h
@@ -11,18 +11,21 @@
 #include <string.h>
 #include <pthread.h>
 #include "System_Specifications.h"
+#include "Airplane.cpp"
 using namespace std;
 
 class data_display{
 	public:
 		data_display();
 		~data_display();
+		vector<Airplane> atcAirplanes;
 	private:
 		pthread_t initializeDataDisplayThread();
 		void *dataDisplayBody(void * arg);
 		void print_airspace();
 		void print_AircraftInfo();
 		void print_Warnings();
+		void updateAirplaneforDataDisplay(Msg2DataDisplay msg);
 };
 
 #endif /* DATA_DISPLAY_H_ */
