7020d92888133105c07cec8e859a76305886242c
Charlie Huang <74630670+CharlieHuang98@users.noreply.github.com>
2023-04-10 21:32:49 -0400

    Updated communication system

--- communication_system.cpp
+++ communication_system.cpp
@@ -16,11 +16,12 @@
 using namespace std;
 
 #include "communication_system.h"
-#include "computer_system.cpp"
+//#include "computer_system.cpp"
 #include "System_Specifications.h"
+#include "airplane.h"
 
 
-#define ATTACH_POINT "communication_system_channel" // change ATTACH_POINT to server name
+//#define ATTACH_POINT "communication_system_channel" // change ATTACH_POINT to server name
 
 /* We specify the header as being at least a pulse */
 /* All of your messages should start with this header */
@@ -31,19 +32,18 @@ using namespace std;
 typedef struct _pulse msg_header_t;
 
 /* Our real data comes after the header */
-typedef struct communication_system_data {
-	msg_header_t hdr;
-	double data;
-	MsgToAirplane type;
-} my_data_t;
-
+//typedef struct communication_system_data {
+//	msg_header_t hdr;
+//	double data;
+//	MsgToAirplane type;
+//} my_data_t;
 
 
 /*
  * Initialization of the Communication Systems thread
  */
 
-pthread_t initializeCommunicationSystemThread(){
+pthread_t InitializeCommunicationSystemThread(){
     int err_no;
 	pthread_t Communication_threadD;
 	pthread_attr_t attr;
@@ -51,19 +51,19 @@ pthread_t initializeCommunicationSystemThread(){
 	int detachstate;
 
 	err_no = pthread_attr_init(&attr);
-	if(err_no)
-		ERROR("ERROR, RC from pthread_attr_init() is %d \n", err_no);
+//	if(err_no)
+//		ERROR("ERROR, RC from pthread_attr_init() is %d \n", err_no);
 
 	// create thread with attributes
 		err_no = pthread_create(&Communication_threadD, &attr, communication_system_main, NULL);
 
-	// detach state
+//	// detach state
 	err_no = pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-	if(err_no)
-			ERROR("ERROR, RC from pthread_attr_init() is %d \n", err_no);
+//	if(err_no)
+//			ERROR("ERROR, RC from pthread_attr_init() is %d \n", err_no);
+//
 
-
-return Communication_threadD;
+	return Communication_threadD;
 }
 
 
@@ -71,7 +71,7 @@ return Communication_threadD;
  *
  * There are still a lot of errors that need to be fixed
  */
-bool isCompSystemMessageGood(int rcvid, my_data_t MsgToCommunicationSystem){
+bool isCompSystemMessageGood(int rcvid, CommonMessage msg){
 
 	/* Do your MsgReceive's here now with the chid */
 		/* Server will block in this call, until a client calls MsgSend to send a message to
@@ -83,14 +83,14 @@ bool isCompSystemMessageGood(int rcvid, my_data_t MsgToCommunicationSystem){
 		}
 
 		if (rcvid == 0) {/* Pulse received */
-			switch (MsgToCommunicationSystem.hdr.code) {
+			switch (msg.hdr.code) {
 			case _PULSE_CODE_DISCONNECT:
 				/*
 				 * A client disconnected all its connections (called
 				 * name_close() for each name_open() of our name) or
 				 * terminated
 				 */
-				ConnectDetach(MsgToCommunicationSystem.hdr.scoid);
+				ConnectDetach(msg.hdr.scoid);
 				break;
 			case _PULSE_CODE_UNBLOCK:
 				/*
@@ -111,13 +111,13 @@ bool isCompSystemMessageGood(int rcvid, my_data_t MsgToCommunicationSystem){
 		}
 
 		/* name_open() sends a connect message, must EOK this */
-		if (MsgToCommunicationSystem.hdr.type == _IO_CONNECT ) {
+		if (msg.hdr.type == _IO_CONNECT ) {
 			MsgReply( rcvid, EOK, NULL, 0 );
 			return false;
 		}
 
 		/* Some other QNX IO message was received; reject it */
-		if (MsgToCommunicationSystem.hdr.type > _IO_BASE && MsgToCommunicationSystem.hdr.type <= _IO_MAX ) {
+		if (msg.hdr.type > _IO_BASE && msg.hdr.type <= _IO_MAX ) {
 			MsgError( rcvid, ENOSYS );
 			return false;
 		}
@@ -140,21 +140,21 @@ bool isCompSystemMessageGood(int rcvid, my_data_t MsgToCommunicationSystem){
 		 * configuratin message could be used to send the period/frequency in which the client would be sending messages.
 		 * Thus, the server could consider that while doing other things. */
 
-		if (MsgToCommunicationSystem.hdr.type == 0x00) {
+		if (msg.hdr.type == Command) {
 			/*A motion message has been received*/
-			if (MsgToCommunicationSystem.hdr.subtype == 0x01) {
+			if (msg.hdr.subtype == Change_Speed) {
 				/* A velocity message has been received  */
-				printf("The velocity of the object is  %.2f m/s \n", MsgToCommunicationSystem.data);
+				printf("The velocity of the object is  %.2f m/s \n", msg.data);
 			}
-			else if (MsgToCommunicationSystem.hdr.subtype == 0x02) {
+			else if (msg.hdr.subtype == Change_Position) {
 				/* A position message has been received  */
-				printf("The x position of the object is  %.2f m \n", MsgToCommunicationSystem.data);
+				printf("The x position of the object is  %.2f m \n", msg.data);
 				printf("\n");
 			}
-			else{
-				MsgError( rcvid, ENOSYS );
-				return false;
-			}
+//			else{
+//				MsgError( rcvid, ENOSYS );
+//				return false;
+//			}
 		}
 
 		MsgReply(rcvid, EOK, 0, 0);
@@ -163,42 +163,38 @@ bool isCompSystemMessageGood(int rcvid, my_data_t MsgToCommunicationSystem){
 }
 
 // issue here on how to send message to airplane since airplane doesn't have a channel
-static bool SendCommandToAirplane(){
-	int server_coid; //server connection ID
-	const char* AirplaneChannel;
-	my_data_t MessageToAirplane;
-
-
-	if ((server_coid = name_open(AirplaneChannel, 0)) == -1) {
-			perror("Error occurred while attaching the channel");
-			return false;
-		}
-
-	if(MsgSend(server_coid, &MessageToAirplane, sizeof(MessageToAirplane), NULL, 0) == -1){
-		printf("Error while sending the position message");
-		return false;
-	}
-
-	/* Close the connection */
-		name_close(server_coid);
-		return true;
-}
+//static bool SendCommandToAirplane(){
+//	int server_coid; //server connection ID
+//	const char* AirplaneChannel;
+//	CommonMessage msg;
+//
+//	if ((server_coid = name_open(AirplaneChannel, 0)) == -1) {
+//			perror("Error occurred while attaching the channel");
+//			return false;
+//		}
+//
+//	if(MsgSend(server_coid, &msg, sizeof(msg), NULL, 0) == -1){
+//		printf("Error while sending the position message");
+//		return false;
+//	}
+//
+//	/* Close the connection */
+//		name_close(server_coid);
+//		return true;
+//}
 
 void* communication_system_main(void* arg){
 	int rcvid;
     name_attach_t *attach;
-    my_data_t msg;
-
-    char CommunicationSystem_Channel[64] = "CommunicationSystemChannel";
-
 
     /* Create a local name (/dev/name/local/...) */
-    if ((attach = name_attach(NULL, ATTACH_POINT, 0)) == NULL) {
-    	ERROR("Error occurred while creating the channel", CommunicationSystem_Channel);
+    if ((attach = name_attach(NULL, COMMUNICATION_CHANNEL, 0)) == NULL) {
+ //   	ERROR("Error occurred while creating the channel", CommunicationSystem_Channel);
     	return NULL;
     }
 
     while (true) {
+    		CommonMessage msg;
     		/* Server will block in this call, until a client calls MsgSend to send a message to
     		 * this server through the channel named "myname", which is the name that we set for the channel,
     		 * i.e., the one that we stored at ATTACH_POINT and used in the name_attach call to create the channel. */
@@ -206,44 +202,97 @@ void* communication_system_main(void* arg){
 
     		if (rcvid == -1) {/* Error condition, exit */
     			break;
-    			}
-    		bool CompSystemMessageGood = isCompSystemMessageGood(rcvid, msg);
+    		}
+ //   		bool CompSystemMessageGood = isCompSystemMessageGood(rcvid, msg);
 
 
     	// here the code should continue to handle the message sent to the airplane but the airplane.cpp doesn't receive messages
 
-    		if(msg.type == ChangeAirplaneSpeed){
-    			MsgToAirplane msgToAirplane;
-    			msgToAirplane.id =  msg.id;
-    			msgToAirplane.type = ChangeAirplaneSpeed;
+    		if(msg.hdr.subtype == Change_Speed){
 
-    			// Set for Coordinates
-    			msgToAirplane.PosX = msg.PosX;
-    			msgToAirplane.PosY = msg.PosY;
-    			msgToAirplane.PosZ = msg.PosZ;
+    			int shm_fd = shm_open("plane2", O_RDWR, 0666);
+    			if (shm_fd != -1){
+    				ftruncate(shm_fd, sizeof(PlaneParams));
+    				void* ptr = mmap(0, sizeof(PlaneParams), PROT_WRITE | PROT_READ, MAP_SHARED, shm_fd, 0);
+    				if (ptr == MAP_FAILED) {
+    					printf("Map failed\n");
+    				}
 
-    			// Set for Speed
-    			msgToAirplane.SpeedX = msg.SpeedX;
-    			msgToAirplane.SpeedY = msg.SpeedY;
-    			msgToAirplane.SpeedZ = msg.SpeedZ;
+    				// SHARED MEMORY OPEN
+
+    				(*((PlaneParams*)ptr)).speedFlag = true;
+    				for (int i = 0; i < 3; i++)
+    					(*((PlaneParams*)ptr)).cmdSpeed[i] = msg.Speed[i];
+
+    				shm_unlink("plane2");
+    				close(shm_fd);
+    			} else {
+    				cout << "error with SH" << endl;
+    			}
 
     			MsgReply(rcvid, EOK, 0, 0);
 
+    		}
+    		else if(msg.hdr.subtype == Change_Position){
 
-    		}else if(msg.type == RequestAirplaneData){
-    			MsgToAirplane msgToAirplane;
-    			msgToAirplane.id =  msg.id;
-    			msgToAirplane.type = RequestAirplaneData;
+    			int shm_fd = shm_open("plane2", O_RDWR, 0666);
+    			if (shm_fd != -1){
+    				ftruncate(shm_fd, sizeof(PlaneParams));
+    				void* ptr = mmap(0, sizeof(PlaneParams), PROT_WRITE | PROT_READ, MAP_SHARED, shm_fd, 0);
+    				if (ptr == MAP_FAILED) {
+    					printf("Map failed\n");
+    				}
+
+    				// SHARED MEMORY OPEN
+
+    				(*((PlaneParams*)ptr)).posFlag = true;
+    				for (int i = 0; i < 3; i++)
+    					(*((PlaneParams*)ptr)).cmdPosition[i] = msg.Position[i];
+
+    				shm_unlink("plane2");
+    				close(shm_fd);
+    			} else {
+    				cout << "error with SH" << endl;
+    			}
 
     			MsgReply(rcvid, EOK, 0, 0);
+
+    		}
+    		else if (msg.hdr.subtype == Request_Plane_Info){
+    			CommonMessage reply;
+
+    			int shm_fd = shm_open("plane2", O_RDWR, 777);
+    			if (shm_fd != -1){
+    				ftruncate(shm_fd, sizeof(PlaneParams));
+    				void* ptr = mmap(0, sizeof(PlaneParams), PROT_WRITE | PROT_READ, MAP_SHARED, shm_fd, 0);
+    				if (ptr == MAP_FAILED) {
+    					printf("Map failed\n");
+    				}
+
+    				reply.plane_id = (*((PlaneParams*)ptr)).id;
+    				for (int i = 0; i < 3; i++) {
+    					reply.Position[i] = (*((PlaneParams*)ptr)).Position[i];
+    					reply.Speed[i] = (*((PlaneParams*)ptr)).Speed[i];
+    				}
+
+    				reply.hdr.type = Command;
+    	    		reply.hdr.subtype = Response_Plane_Info;
+
+    				shm_unlink("plane2");
+    				close(shm_fd);
+    				MsgReply(rcvid, EOK, &reply, sizeof(reply));
+
+    			} else {
+    				cout << "error with SH" << endl;
+        			MsgReply(rcvid, EOK, 0, 0);
+
+    			}
 
 
     		} else{
     			MsgReply(rcvid, EOK, 0, 0);
     		}
-    		return EXIT_SUCCESS;
     }
 
+    return EXIT_SUCCESS;
 }
-
-
